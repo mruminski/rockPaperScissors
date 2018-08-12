1f38e272c5ff2c5e677836c9d4039429e328c62c
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-08-12 17:31:06 +0200

    Add random move

--- js/script.js
+++ js/script.js
@@ -15,7 +15,21 @@ var rock = document.getElementById('rock');
 var paper = document.getElementById('paper');
 var scissors = document.getElementById('scissors');
 
+var playerMove = function(move) {
+  var random = Math.floor((Math.random() * 3) + 1);
+  var aiMove;
 
+  if (random == 1) {
+    aiMove = 'paper';
+  }
+  else if (random == 2) {
+    aiMove = 'rock';
+  } else {
+    aiMove = 'scissors';
+  }
+
+
+}
 
 
 rock.addEventListener('click', playerMove('rock'));
